ad1b97e5583e937e089465abe9ba9a52b0693cc8
Davey Proctor <david.proctor@yale.edu>
2019-04-20 17:06:46 -0400

    Unit tests for classes in blockchain.py

---
+++ test_Blockchain.py
@@ -0,0 +1,27 @@
+# Unit tests for Blockchain class of blockchain.py
+import blockchain
+difficulty=16
+
+block0 = blockchain.Block(0, '0', 'Genesis Block')
+block0.search_hash(difficulty)
+block1 = blockchain.Block(1, block0.hash, 'data1')
+block1.search_hash(difficulty)
+block2 = blockchain.Block(2, block1.hash, 'data2')
+block2.search_hash(difficulty)
+
+
+bchain = blockchain.Blockchain(difficulty)
+assert bchain.validate(block2, block1)
+assert bchain.validate(block1, block0)
+assert bchain.validate(block0, "No_parent")
+assert bchain.validate(block2, block0) == False
+
+assert bchain.add_block(block0)
+assert bchain.add_block(block1)
+assert bchain.add_block(block2)
+
+assert bchain.validate_chain()
+
+
+
+
---
+++ test_DistributedBlockchain.py
@@ -0,0 +1,16 @@
+# Unit tests for DistributedBlockchain class of blockchain.py
+import blockchain
+difficulty=16
+
+distrBChain = blockchain.DistributedBlockchain(difficulty)
+distrBChain._genesis()
+distrBChain._generate()
+distrBChain._generate()
+assert len(distrBChain.chain) == 3
+assert distrBChain.validate_chain()
+
+# Serialize tests
+distrBChain2 = blockchain.DistributedBlockchain(difficulty)
+distrBChain2.deserialize_chain(distrBChain.serialize_chain())
+assert distrBChain2.validate_chain()
+assert str(distrBChain) == str(distrBChain2)
